a6dfa856d7d0ec421ec112cc7351679c11cd4b6f
sorinvisan89 <sorin_andrei_visan@yahoo.com>
2021-12-17 13:52:52 +0200

    initial commit

---
+++ server/src/main/java/com/sorin/playground/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.sorin.playground.controller;
+
+import com.sorin.playground.api.UserApi;
+import com.sorin.playground.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class UserController implements UserApi {
+
+    @Override
+    public ResponseEntity<Void> createUser(User body) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteUser(Integer userId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<User> getUserById(Integer userId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<Void> updateUser(Integer userId, User body) {
+        return ResponseEntity.ok().build();
+    }
+}
